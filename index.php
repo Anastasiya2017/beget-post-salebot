9541bdc857d3430a0eb4e941e2229d3f81f43195
Asya_moon <alleksa_anastasiya@mail.ru>
2023-12-17 15:29:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $request = $_POST;
+
+    const URL = ''; // url запроса
+
+    // параметры запроса
+    $params = [
+        'endDate' => '',
+        'isLocked' => '',
+        // ...
+    ];
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, URL);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    header('Content-Type: application/json');
+    echo $response;
+    exit();
